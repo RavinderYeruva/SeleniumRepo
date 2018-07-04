99755c7e1d1a7d1c2ccc5b6102f6d3327472f587
Ravinder Yeruva <ravinder.r.yeruva@gmail.com>
2018-07-04 05:47:29 +0530

    test3

--- SelVerifyLogin/src/test/java/VerifyLoginTestNG/VerifyLoginTest.java
+++ SelVerifyLogin/src/test/java/VerifyLoginTestNG/VerifyLoginTest.java
@@ -19,12 +19,12 @@ public class VerifyLoginTest {
 	  System.setProperty("webdriver.chrome.driver","//home//edureka//SelVerifyLogin//chromedriver");
 	  ChromeOptions options = new ChromeOptions();
 	  options.setExperimentalOption("useAutomationExtension", false);
-	  //options.addArguments("--headless");
+	  //options.setHeadless(true);
 	  //options.addArguments("start-maximized"); // open Browser in maximized mode
 	  //options.addArguments("disable-infobars"); // disabling infobars
-	  //options.addArguments("--disable-extensions"); // disabling extensions
-	  //options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-	  //options.addArguments("--no-sandbox"); 
+	  options.addArguments("--disable-extensions"); // disabling extensions
+	  options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+	  options.addArguments("--no-sandbox"); 
 	  driver = new ChromeDriver(options);
 	  driver.get("https://github.com/login");
 	  driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
